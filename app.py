7f5679cc898167b10876847c28b28f0f910d52a5
Ilyas Rehouni <ilyas.rehouni@gmail.com>
2025-04-03 16:18:14 +0200

    Query tests

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
+import os
+import urllib
 from flask import Flask, render_template
+from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
+
+# Set up the connection string from an environment variable or hardcode it for testing
+raw_connection_string = os.environ.get('DATABASE_URL')
+if raw_connection_string is None:
+    raw_connection_string = (
+        "Driver={ODBC Driver 18 for SQL Server};"
+        "Server=tcp:your-server.database.windows.net,1433;"
+        "Database=sustainabite-database;"
+        "Uid=sustainabite-server-admin;"
+        "Pwd={Qvs6YFz6AN$ekYvN};"
+        "Encrypt=yes;"
+        "TrustServerCertificate=no;"
+        "Connection Timeout=30;"
+    )
 
 @app.route('/')
 def home():
